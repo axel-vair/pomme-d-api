eee8fc99b442dda0ccf494022b58eb145696a373
Axel <axel.vair@laplateforme.io>
2024-03-11 19:55:50 +0100

    Ajout d'une URL pour fetch tous les produits de l'API

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Service\CallApiService;
 use http\Client;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+
+    #[Route('/products', name: 'app_products')]
+    public function allProducts(\Symfony\Component\HttpFoundation\Request $request, CallApiService $callApiService): Response
     {
+        $page = $request->query->getInt('page', 1);
+        $products = $callApiService->getAllProducts($page);
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+            'currentPage' => $page
+        ]);
     }
+
 }
---
+++ src/Service/CallApiService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CallApiService
+{
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getAllProducts($page = 1): array
+    {
+        $response = $this->client->request('GET', 'https://world.openfoodfacts.net/api/v2/search', [
+            'query' => [
+                'page_size' => 1000,
+                'page' => $page,
+                'fields' => 'product_name,nutrition_grades',
+            ]
+        ]);
+
+        return $response->toArray();
+    }
+}
